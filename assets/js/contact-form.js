da965af34812bd43ff328b40cc1c1eb52bef8168
Aleksandra Haniok <a.haniok@gmail.com>
2021-12-27 17:53:21 +0100

    Fix contact form to send all input data to email

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,17 +1,32 @@
 /**
  * Add an eventListener to listen for the submit.
  * Sends an email to site owner through emailJS if the submit is fired.
- * Script taken from the official EmailJS tutorial https://www.emailjs.com/docs/tutorial/creating-contact-form/
+ * Script taken from the official EmailJS tutorial https://www.emailjs.com/docs/tutorial/creating-contact-form/ 
+ * and Email Templates Playground environment.
  */
+const sendFormButton = document.getElementById("btn-send-form");
 
 document.getElementById("contact-form").addEventListener("submit", function (event) {
     event.preventDefault();
-    // these IDs from the previous steps
     emailjs.init("user_ky3jHn7C0IfrC1dfTWauP");
+    sendFormButton.value = "Sending..."; //changing value of the button when sending in progress
+
     emailjs.sendForm("sunshineguessing", "sgg-contact-form", this)
-        .then(function () {
-            console.log("SUCCESS!");
-        }, function (error) {
-            console.log("FAILED...", error);
+        .then(() => {
+            sendFormButton.value = "Send";
+            formSubmittedMessage();
+        }, (err) => {
+            console.log(JSON.stringify(err));
         });
 });
+
+
+function formSubmittedMessage() {
+    let message = `
+            <div class="image-404">
+                <img src="assets/images/contact-form-thankyou-image.png" alt="Sun image with thank you writing">
+            </div>
+            <a href="index.html" class="button my-3">Go back</a>
+            `;
+    document.getElementById("game-container").innerHTML = message;
+}
