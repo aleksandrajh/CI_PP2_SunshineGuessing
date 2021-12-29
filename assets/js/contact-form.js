ff5d26542c266b262a90869e9e86131926cc2253
Aleksandra Haniok <a.haniok@gmail.com>
2021-12-29 10:19:47 +0100

    Test with mobile keyboard pushing the form off

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+    document.ontouchmove = function (e) {
+        e.preventDefault();
+    }
+});
+
 input.onfocus = function () {
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
